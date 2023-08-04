a6e3aa251c60b2a2321b3e4aa9471165e07eb5af
Skarsaif <123283358+Skarsaif@users.noreply.github.com>
2023-08-04 12:08:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,4 +27,12 @@ function addTask() {
     const taskItem = button.parentNode;
     taskItem.remove();
   }
-  
+  setInterval(myFunction, 1000);
+
+function myFunction() {
+  let d = new Date();
+  document.getElementById("demo").innerHTML=
+  d.getHours() + ":" +
+  d.getMinutes() + ":" +
+  d.getSeconds();
+}

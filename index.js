c5340f75f767806a361d635460ba4d0604c45b4f
Skarsaif <123283358+Skarsaif@users.noreply.github.com>
2023-08-04 12:07:28 +0530

    Update index.js
    Timer

--- index.js
+++ index.js
@@ -27,4 +27,13 @@ function addTask() {
     const taskItem = button.parentNode;
     taskItem.remove();
   }
+setInterval(myFunction, 1000);
+
+function myFunction() {
+  let d = new Date();
+  document.getElementById("demo").innerHTML=
+  d.getHours() + ":" +
+  d.getMinutes() + ":" +
+  d.getSeconds();
+}
   

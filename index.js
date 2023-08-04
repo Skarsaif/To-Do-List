b0debebee931543b2900369c6f7b0c91381df81c
Skarsaif <123283358+Skarsaif@users.noreply.github.com>
2023-08-04 12:11:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,10 +29,3 @@ function addTask() {
   }
   setInterval(myFunction, 1000);
 
-function myFunction() {
-  let d = new Date();
-  document.getElementById("demo").innerHTML=
-  d.getHours() + ":" +
-  d.getMinutes() + ":" +
-  d.getSeconds();
-}
